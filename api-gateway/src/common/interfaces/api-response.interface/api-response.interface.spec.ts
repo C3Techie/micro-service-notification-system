981a5ef445a84ece8300791209fbd46e0647a17e
Awesome6192 <christianchibuike14@gmail.com>
2025-11-11 21:27:16 +0100

    chore: create api gateway project structure

---
+++ api-gateway/src/common/interfaces/api-response.interface/api-response.interface.spec.ts
@@ -0,0 +1,7 @@
+import { ApiResponseInterface } from './api-response.interface';
+
+describe('ApiResponseInterface', () => {
+  it('should be defined', () => {
+    expect(new ApiResponseInterface()).toBeDefined();
+  });
+});
---
+++ api-gateway/src/notifications/dto/notification-response.dto/notification-response.dto.spec.ts
@@ -0,0 +1,7 @@
+import { NotificationResponseDto } from './notification-response.dto';
+
+describe('NotificationResponseDto', () => {
+  it('should be defined', () => {
+    expect(new NotificationResponseDto()).toBeDefined();
+  });
+});
